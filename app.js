a3abe41fc536e30b796206b55431bb58daa6c2b6
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2022-12-04 00:28:57 +0200

    Modified styles and compose

--- app.js
+++ app.js
@@ -13,8 +13,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+let posts = [];
+
 app.get('/', (req, res) => {
-  res.render('home', {homeStartingContent: homeStartingContent});
+  res.render('home', {
+    homeStartingContent: homeStartingContent,
+    postData: posts
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -30,7 +35,13 @@ app.get('/compose', (req, res) => {
 });
 
 app.post('/compose', (req, res) => {
-  console.log();
+  let post = {
+    title: req.body.composeInput,
+    body: req.body.composeBody
+  };
+  posts.push(post);
+
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
